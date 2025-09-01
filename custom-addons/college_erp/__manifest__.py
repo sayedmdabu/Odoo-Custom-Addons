a8ac81b9364b69e080ce73c3e723c84d9aa7c448
sayed <sayedmdabu94@gmail.com>
2025-09-01 18:04:39 +0600

    college ERP learn

---
+++ custom-addons/college_erp/__manifest__.py
@@ -0,0 +1,21 @@
+{
+    "name": "College ERP",
+    "version": "18.0.1.0.1",
+    "summary": "An ERP for college education",
+    "description": """
+From student administration to exams, this module covers all aspects of college management.
+    """,
+    "author": "SAGBRAIN CORPORATION / Md Abu Sayed",
+    "website": "https://sagbrain.com/",
+    "license": "LGPL-3",
+    "category": "Education",
+    "sequence": 1,
+    "depends": ["base"],
+    "data": [
+        # XML/CSV files go here (views, security, data, etc.)
+    ],
+    "images": ["static/description/icon.png"],
+    "application": True,
+    "installable": True,
+    "auto_install": False,
+}
---
+++ custom-addons/college_erp/models/college_student.py
@@ -0,0 +1,5 @@
+from odoo import models
+
+class CollegeStudent(models.Model):
+    _name = 'college.student'
+    _description = 'College Student'

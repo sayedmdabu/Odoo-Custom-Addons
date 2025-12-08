73882a222a10d7f62383103ec50654e481c5c705
sayed <sayedmdabu94@gmail.com>
2025-12-08 08:46:04 +0600

    new addons product_auto_route_supplier

--- custom-addons/product_auto_route_supplier/__manifest__.py
+++ custom-addons/product_auto_route_supplier/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'Product Auto Route & Supplier Management',
-    'version': '18.0..0.1',
+    'version': '18.0.1.0.0',
     'category': 'Inventory/Purchase',
     'summary': 'Automatic routing, vendor mapping, purchase price rules, and sales discount automation',
     'description': """
